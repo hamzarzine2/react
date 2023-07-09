1c8108d55ec6093a4c0ef644984d241e9809629f
hamzarzine2 <hamza.rzine@student.vinci.be>
2023-07-09 16:48:46 +0200

    exo 1.1-1.2

---
+++ exercices/module1/exo1/src/content.js
@@ -0,0 +1,16 @@
+import Part from "./Part";
+
+const Content = (table)=>{
+    console.log(table);
+    return (
+        <>
+            <Part part={table.table[0].part} exercices={table.table[0].exercices} />
+            <Part part={table.table[1].part} exercices={table.table[1].exercices} />
+            <Part part={table.table[2].part} exercices={table.table[2].exercices} />
+        </>
+
+
+    );
+}
+
+export default Content;
---
+++ exercices/module1/exo1/src/header.js
@@ -0,0 +1,10 @@
+const Header = (courseName) => {
+    console.log(courseName);
+    return(
+        <>
+        <h1>{courseName.name}</h1>
+        </>
+    );
+}
+
+export default Header;
